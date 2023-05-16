aa8f671d51e143adc071ef18e300013f92a27dea
Dylan Miller <dmanmiller2004@gmail.com>
2023-05-16 17:11:37 -0400

    Reduce tablelayoutpanel to 3 rows

--- Display test/Form1.Designer.cs
+++ Display test/Form1.Designer.cs
@@ -115,22 +115,19 @@ namespace Display_test
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8F));
-            this.tableLayoutPanel1.Controls.Add(this.btnHistory, 3, 4);
-            this.tableLayoutPanel1.Controls.Add(this.btnPartners, 2, 4);
-            this.tableLayoutPanel1.Controls.Add(this.btnPrograms, 1, 4);
-            this.tableLayoutPanel1.Controls.Add(this.picLogo, 3, 1);
-            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnHistory, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnPartners, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnPrograms, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.picLogo, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 2, 0);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 7;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1370, 749);
             this.tableLayoutPanel1.TabIndex = 6;
@@ -145,7 +142,7 @@ namespace Display_test
             this.btnHistory.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnHistory.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnHistory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(30)))), ((int)(((byte)(66)))));
-            this.btnHistory.Location = new System.Drawing.Point(878, 353);
+            this.btnHistory.Location = new System.Drawing.Point(878, 227);
             this.btnHistory.Name = "btnHistory";
             this.btnHistory.Size = new System.Drawing.Size(377, 293);
             this.btnHistory.TabIndex = 37;
@@ -162,7 +159,7 @@ namespace Display_test
             this.btnPartners.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnPartners.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPartners.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(30)))), ((int)(((byte)(66)))));
-            this.btnPartners.Location = new System.Drawing.Point(495, 353);
+            this.btnPartners.Location = new System.Drawing.Point(495, 227);
             this.btnPartners.Name = "btnPartners";
             this.btnPartners.Size = new System.Drawing.Size(377, 293);
             this.btnPartners.TabIndex = 36;
@@ -179,7 +176,7 @@ namespace Display_test
             this.btnPrograms.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnPrograms.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPrograms.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(30)))), ((int)(((byte)(66)))));
-            this.btnPrograms.Location = new System.Drawing.Point(112, 353);
+            this.btnPrograms.Location = new System.Drawing.Point(112, 227);
             this.btnPrograms.Name = "btnPrograms";
             this.btnPrograms.Size = new System.Drawing.Size(377, 293);
             this.btnPrograms.TabIndex = 35;
@@ -193,10 +190,10 @@ namespace Display_test
             this.tableLayoutPanel1.SetColumnSpan(this.picLogo, 2);
             this.picLogo.Dock = System.Windows.Forms.DockStyle.Right;
             this.picLogo.Image = global::Display_test.Properties.Resources.marketinglogo;
-            this.picLogo.Location = new System.Drawing.Point(1036, 37);
+            this.picLogo.Location = new System.Drawing.Point(1036, 0);
             this.picLogo.Margin = new System.Windows.Forms.Padding(0);
             this.picLogo.Name = "picLogo";
-            this.picLogo.Size = new System.Drawing.Size(334, 157);
+            this.picLogo.Size = new System.Drawing.Size(334, 224);
             this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picLogo.TabIndex = 34;
             this.picLogo.TabStop = false;
@@ -210,12 +207,12 @@ namespace Display_test
             this.tableLayoutPanel3.Controls.Add(this.lblTouch, 0, 0);
             this.tableLayoutPanel3.Controls.Add(this.pictureBox1, 1, 0);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(492, 37);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(492, 0);
             this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 1;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(383, 157);
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(383, 224);
             this.tableLayoutPanel3.TabIndex = 7;
             // 
             // lblTouch
@@ -228,7 +225,7 @@ namespace Display_test
             this.lblTouch.Location = new System.Drawing.Point(0, 0);
             this.lblTouch.Margin = new System.Windows.Forms.Padding(0);
             this.lblTouch.Name = "lblTouch";
-            this.lblTouch.Size = new System.Drawing.Size(229, 157);
+            this.lblTouch.Size = new System.Drawing.Size(229, 224);
             this.lblTouch.TabIndex = 30;
             this.lblTouch.Text = "Touch Screen to Interact";
             this.lblTouch.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -242,7 +239,7 @@ namespace Display_test
             this.pictureBox1.Location = new System.Drawing.Point(249, 20);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(20);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(114, 117);
+            this.pictureBox1.Size = new System.Drawing.Size(114, 184);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
