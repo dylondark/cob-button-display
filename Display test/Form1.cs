0386cd0b16354ae9000766b21a666ccb95f6b7e9
Dylan Miller <dmanmiller2004@gmail.com>
2023-05-25 16:10:19 -0400

    Clean up autoscale function

--- Display test/Form1.cs
+++ Display test/Form1.cs
@@ -61,11 +61,11 @@ namespace Display_test
         private void autoScale(object sender, EventArgs e)
         {
             const int scaleFactor = 60; // based on the desired font size for the welcome text on a 4k display, everything else will scale accordingly with this
-            float resFactor = Convert.ToSingle(this.Width) / 3840f; // this will be 1 at 4k
+            float resFactor = (this.Width) / 3840f; // this will be 1 at 4k
 
-            Font labelFont = new Font(new FontFamily("Calibri"), scaleFactor * resFactor, FontStyle.Bold); ;
+            Font labelFont = new Font(new FontFamily("Calibri"), scaleFactor * resFactor, FontStyle.Bold);
             lblTouch.Font = labelFont;
-            picTouch.Margin = new Padding(Convert.ToInt32(Math.Ceiling(scaleFactor * resFactor)));
+            picTouch.Margin = new Padding((int)(Math.Ceiling(scaleFactor * resFactor)));
 
             Font socialFont = new Font(new FontFamily("Calibri"), scaleFactor * resFactor * 0.6f, FontStyle.Bold);
             lblIG.Font = socialFont;
@@ -73,26 +73,26 @@ namespace Display_test
             lblFB.Font = socialFont;
 
             lblHashtags.Font = socialFont;
-            lblHashtags.Padding = new Padding(Convert.ToInt32(Math.Ceiling(scaleFactor * resFactor * 0.3f)));
+            lblHashtags.Padding = new Padding((int)(Math.Ceiling(scaleFactor * resFactor * 0.3f)));
 
-            Padding buttonMargin = new Padding(Convert.ToInt32(Math.Ceiling(scaleFactor * 0.2f * resFactor)));
+            Padding buttonMargin = new Padding((int)(Math.Ceiling(scaleFactor * 0.2f * resFactor)));
             btnAlumni.Margin = buttonMargin;
             btnExpBusiness.Margin = buttonMargin;
             btnSocialMedia.Margin = buttonMargin;
 
-            picTouch.Margin = new Padding(Convert.ToInt32(Math.Ceiling(scaleFactor * resFactor)));
-            int picSize = Convert.ToInt32(Math.Ceiling(scaleFactor * resFactor * 12f));
+            picTouch.Margin = new Padding((int)(Math.Ceiling(scaleFactor * resFactor)));
+            int picSize = (int)(Math.Ceiling(scaleFactor * resFactor * 12f));
             picLogo.Size = new Size(picSize, picSize);
 
             // back buttons scaling
-            int buttonDim = Convert.ToInt32(scaleFactor * 3f * resFactor); 
+            int buttonDim = (int)(scaleFactor * 3f * resFactor); 
             btnBack.Width = buttonDim;
             btnBack.Height = buttonDim;
             btnHome.Width = buttonDim;
             btnHome.Height = buttonDim;
 
             // back button positions
-            int edgeDistance = Convert.ToInt32(scaleFactor * 2f * resFactor); // distance in pixels from edges of screen for back buttons
+            int edgeDistance = (int)(scaleFactor * 2f * resFactor); // distance in pixels from edges of screen for back buttons
             int[] buttonPos = new int[] {this.Width - edgeDistance - buttonDim, this.Height - edgeDistance - buttonDim};
             btnBack.Left = buttonPos[0];
             btnBack.Top = buttonPos[1];
